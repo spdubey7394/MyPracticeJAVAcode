22e155554fdf91088aee503e8898faa6fcee7393
spdub <spdub@Surya>
2025-01-06 23:22:45 +0530

    4th commit

--- src/arrademo/SortingCharacterUsingArray.java
+++ src/arrademo/SortingCharacterUsingArray.java
@@ -8,12 +8,19 @@ public class SortingCharacterUsingArray {
 	{
 		char ch[]= {'f','s','x','c','q','w'};
 		
+		String S[]= {"Gucci","LouisVittion","Armani","Rolex","Pagani"};
+		
 		System.out.println("Characters before sorting" + Arrays.toString(ch));
 		
 		Arrays.sort(ch);
 		
 		System.out.println("Characters after sorting" + Arrays.toString(ch));
 
+        System.out.println("STRING before sorting" + Arrays.toString(S));
+		
+		Arrays.sort(S);
+		
+		System.out.println("STRING after sorting" + Arrays.toString(S));
 	}
 
 }
