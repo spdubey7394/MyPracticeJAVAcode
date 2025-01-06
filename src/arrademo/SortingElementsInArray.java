94ed4b59244edbfa29edbfe93ef0caaad61d7cb7
spdub <spdub@Surya>
2025-01-06 22:44:29 +0530

    2nd commit

---
+++ src/arrademo/SortingElementsInArray.java
@@ -0,0 +1,22 @@
+package arrademo;
+import java.util.*;
+
+public class SortingElementsInArray 
+{
+	public static void main(String[] args)
+	{
+		int a[]= {1,121,45,89,321,65,97,55,77,12,2,9};
+		
+		System.out.println("Before Sorting..." + Arrays.toString(a));
+		
+		Arrays.sort(a);
+		
+		System.out.println("After Sorting..." + Arrays.toString(a));
+		
+		
+			
+		
+
+	}
+
+}
